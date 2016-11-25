b05e7eaa66cab187ce1933a22f1a0859c1e8fd2f
Oskiii <oliukku@gmail.com>
2016-11-25 20:37:12 +0200

    walls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,10 +8,15 @@ public class Player : MonoBehaviour, IDamageable {
 	private int health;
 	public float moveSpeed;
 	[SerializeField] private GameObject Character;
+	private Rigidbody2D rb;
+
+	void Start(){
+		rb = GetComponent<Rigidbody2D> ();
+	}
 
 	void Update(){
 		moveDir = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical")).normalized;
-		transform.Translate (moveDir * moveSpeed);
+		rb.velocity = (moveDir * moveSpeed);
 
 		if(moveDir != Vector2.zero)
 			Character.transform.up = -moveDir;
