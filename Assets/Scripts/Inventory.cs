1aef5bc61642a07b89bbe75ad75b725805b42f31
Saneri <santeri.suitiala@kolumbus.fi>
2016-11-25 22:52:26 +0200

    PowerUps!

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+
+	private List<PowerUp> powerUps;
+    private int nofPowerUps = 0;
+
+    void start()
+    {
+        powerUps = new List<PowerUp>() {null, null, null, null};
+    }
+
+	public void Add(PowerUp powerup) {
+		if (nofPowerUps < 4) {
+            powerUps.Add(powerup);
+            nofPowerUps += 1;
+        }
+	}
+
+	public void Use(int button) { // 0 = O, 1 = kolmio, 2 = nelio, 3 = x
+        switch(button) {
+            case 0:
+                if (powerUps[0] != null)
+                {
+                    //use?
+                    powerUps[0] = null;
+                }
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+	}
+}
---
+++ Assets/Scripts/Shield.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shield : MonoBehaviour, PowerUp {
+	
+	private int amount;
+
+	public void PickUp(Player player) {
+		
+		player.AddHealth (amount);
+		Destroy (gameObject);
+	}
+
+    public void Use(Player player)
+        //not an usable item
+    {
+
+    }
+
+}
