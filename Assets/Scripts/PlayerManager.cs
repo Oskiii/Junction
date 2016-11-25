48b4d61b9f5481fb998b30619f52e7bd485f1dbc
Oskiii <oliukku@gmail.com>
2016-11-25 21:18:42 +0200

    ball spawner WIP

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerManager : MonoBehaviour {
+
+	[SerializeField] private GameObject playerObject;
+	public int PlayerAmount;
+	public List<Player> PlayerObjects;
+
+	void Start () {
+		for (int i = 0; i < PlayerAmount; i++) {
+			Instantiate (playerObject);
+		}
+	}
+
+	void Update () {
+	
+	}
+}
