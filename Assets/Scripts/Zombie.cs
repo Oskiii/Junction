71a553557bed824b7c1a2d4bfac6863c123ee0ea
Miika Kanerva <miika.kanerva@aalto.fi>
2016-11-25 20:48:54 +0200

    Zombie class

---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Zombie : MonoBehaviour, IDamageable {
+    public void TakeDamage(int amount)
+    
+    {
+        throw new NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
