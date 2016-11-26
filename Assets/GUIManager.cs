a8c034cb185b62354c0e414ef0a9b20ed971ca9c
Oskiii <oliukku@gmail.com>
2016-11-26 13:21:10 +0200

    guimanager

---
+++ Assets/GUIManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GUIManager : MonoBehaviour {
+
+	[SerializeField] private List<Image> inventorySlots;
+
+	void SetInventoryImage(int slot, Sprite spr){
+		inventorySlots [slot].sprite = spr;
+	}
+}
